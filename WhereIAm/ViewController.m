71f72ca477cf9e4cb52478be075d55be01867547
Rafal <rafal.wsl@gmail.com>
2014-10-05 15:52:53 +0100

    First commit

---
+++ WhereIAm/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WhereIAm
+//
+//  Created by Rafal Wesolowski on 05/10/2014.
+//  Copyright (c) 2014 Rafal Wesolowski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ WhereIAm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WhereIAm
+//
+//  Created by Rafal Wesolowski on 05/10/2014.
+//  Copyright (c) 2014 Rafal Wesolowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
